3889286901826ee5ee07470d7af1bce0886ef155
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2023-12-01 09:25:11 +0700

    Initial Commit

---
+++ DimzPokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DimzPokeDex
+//
+//  Created by Dimas Aristyo Rahadian on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
